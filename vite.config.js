51958ad0b685530d8f6b56fca89e088a7871d671
jji-102 <51739271+jji-102@users.noreply.github.com>
2026-03-17 14:30:37 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/panel-report-dashboard-2025/', // <-- ใส่ชื่อ Repo ของคุณตรงนี้ (ต้องมี / ปิดหน้าหลัง)
+  base: './', // <-- ใส่ชื่อ Repo ของคุณตรงนี้ (ต้องมี / ปิดหน้าหลัง)
 })
